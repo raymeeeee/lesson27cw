5d85411b648cc773f4830a7f281d54db1a4dc4fb
raymeeeee <daniapoluektov@gmail.com>
2024-09-30 15:38:58 +0100

    create use of Printer in main

--- lesson27cw/Printer.cpp
+++ lesson27cw/Printer.cpp
@@ -4,7 +4,7 @@ void Printer::printFile(string text, string fileName, int copy)
 {
 	for (int i = 0; i < copy; i++)
 	{
-		ofstream file("Copy#"+to_string(i)+" " + fileName + "\n");
+		ofstream file("Copy#"+to_string(i) + fileName);
 		if (file.is_open()) {
 			file << text;
 			file.close();
--- lesson27cw/lesson27cw.cpp
+++ lesson27cw/lesson27cw.cpp
@@ -1,15 +1,17 @@
 #include "Scanner.h"
-
+#include "Printer.h"
 int main()
 {
-    Scanner a;
-    cout << "Info:\n" << a.getText() << endl;
-    string user;
-    cout << "Input file name: ";
-    cin >> user;
-    a.scanFile(user);
-    cout << "===========================" << endl;
-    cout << "Info:\n" << a.getText() << endl;
+    //Scanner a;
+    //cout << "Info:\n" << a.getText() << endl;
+    //string user;
+    //cout << "Input file name: ";
+    //cin >> user;
+    //a.scanFile(user);
+    //cout << "===========================" << endl;
+    //cout << "Info:\n" << a.getText() << endl;
+
+    Printer::printFile("Hello world!", "file2.txt", 3);
     
 }
 
