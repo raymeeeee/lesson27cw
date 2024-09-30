654c50af7805bbd2b1c5e16eda417915dd8f0294
raymeeeee <daniapoluektov@gmail.com>
2024-09-30 15:23:30 +0100

    create use of Scanner in main

--- lesson27cw/lesson27cw.cpp
+++ lesson27cw/lesson27cw.cpp
@@ -1,7 +1,15 @@
-
+#include "Scanner.h"
 
 int main()
 {
+    Scanner a;
+    cout << "Info:\n" << a.getText() << endl;
+    string user;
+    cout << "Input file name: ";
+    cin >> user;
+    a.scanFile(user);
+    cout << "===========================" << endl;
+    cout << "Info:\n" << a.getText() << endl;
     
 }
 
